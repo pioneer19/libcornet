5e9095111dcf35f496a9e5303dcf93c1b35c5d32
Alex Syrnikov <pioneer19@post.cz>
2020-04-07 13:30:09 +0300

    changed license to Apache-2.0 (from LGPLv3+)

--- libcornet/tls/parser.hpp
+++ libcornet/tls/parser.hpp
@@ -1,20 +1,8 @@
 /*
  * Copyright 2020 Alex Syrnikov <pioneer19@post.cz>
+ * SPDX-License-Identifier: Apache-2.0
  *
- * This file is part of libcornet.
- *
- *  libcornet is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  libcornet is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with libcornet.  If not, see <https://www.gnu.org/licenses/>.
+ * This file is part of libcornet (https://github.com/pioneer19/libcornet).
  */
 
 #pragma once
